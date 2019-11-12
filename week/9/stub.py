2db877032bb812d7b51c050cca5771068154178a
root <root@localhost.localdomain>
2019-11-11 22:53:10 -0500

    updating

--- week/9/stub.py
+++ week/9/stub.py
@@ -2,7 +2,8 @@
 
 import sys
 import struct
-
+import base64
+from datetime import datetime
 
 # You can use this method to exit on failure conditions.
 def bork(msg):
@@ -14,7 +15,7 @@ MAGIC = 0x8BADF00D
 VERSION = 1
 
 if len(sys.argv) < 2:
-    sys.exit("Usage: python stub.py input_file.fpff")
+    bork("Usage: python stub.py input_file.fpff")
 
 # Normally we'd parse a stream to save memory, but the FPFF files in this
 # assignment are relatively small.
@@ -25,6 +26,17 @@ with open(sys.argv[1], 'rb') as fpff:
 # Hint: you might find it easier to use an index/offset variable than
 # hardcoding ranges like 0:8
 magic, version = struct.unpack("<LL", data[0:8])
+timestamp, author= struct.unpack("<L8s", data[8:20])
+(section,)=struct.unpack("L", data[20:24])
+if section<0:
+    bork("invalid section count")
+else:
+    print("valid time")
+
+try:
+    realts= datetime.fromtimestamp(timestamp)
+except ValueError:
+    bork("invalid timestamp")
 
 if magic != MAGIC:
     bork("Bad magic! Got %s, expected %s" % (hex(magic), hex(MAGIC)))
@@ -35,8 +47,97 @@ if version != VERSION:
 print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
+print("TIMESTAMP: %s" % timestamp)
+print("AUTHOR: %s" % author)
+print("SECTION: %d" % section)
 
 # We've parsed the magic and version out for you, but you're responsible for
 # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+offset = 24
+i = 0
+
+
+
+while i < int(section):
+
+    stype, slen = struct.unpack("<LL", data[offset:offset+8])
+    slen = int(slen)
+    offset=offset+8
+    if stype == 1:
+        (output,) = struct.unpack(("<%ds" % slen), data[offset: (offset + slen)])
+        print("ASCII OUTPUT: %s" % (output))
+        
+    elif stype == 2:
+        (output,) = struct.unpack(("<%ds" % slen), data[offset : (offset + slen)])
+        output = output.decode('utf-8')
+        print("UTF-8 OUTPUT: %s" % (output))
+        
+    elif stype == 3:
+        (output,) = struct.unpack("<" + ("%s" % 'L' * int(slen/4)), data[offset : (offset  + slen)])
+        print("WORDS OUTPUT: %s" % (output))
+        
+    elif stype == 4:
+        (output,) = struct.unpack("<" + ("%s" % 'L' * int(slen/8)), data[offset : (offset + slen)])
+        print("DWORDS OUTPUT: %s" % (output))
+
+    elif stype == 5:
+        (output,) = struct.unpack("<" + ("%s" % 'L' * int(slen/8)), data[offset : (offset + slen)])
+        print("DOUBLES OUTPUT: %s" % (output))
+
+    elif stype == 6:
+        if slen == 16:
+            output = struct.unpack("<dd", data[offset : (offset + slen)])
+
+            x_coord = output[0]
+            y_coord = output[1]
+
+            if (x_coord > 180) or (x_coord < -180) or (y_coord > 180) or (y_coord < -180):
+                bork("BAD COORDS")
+            else:
+                print("COORDS OUTPUT: %s" % str(output))
+        else:
+            bork("WRONG SIZE FOR COORDS")
+
+    elif stype == 7:
+        if slen == 4:
+            output = struct.unpack("<L", data[offset : (offset + slen)])
+            print("REFRENCE OUTPUT: %d" % output[0])
+        else:
+            bork("WORNG SIZE FOR REFRENCE")            
+    elif stype == 8:
+        png_magic = [137, 80, 78, 71, 13, 10, 26, 10]
+        output = struct.unpack("<" + ("%s" % 'B' * (slen)), data[offset : (offset + slen)])
+
+        png = png_magic + list(output)
+
+    	image = open("pngimage.png", "wb")
+    	image.write(bytearray(png))
+   	print ("Image/gif generated %s" % "pngimage.png")
+
+    elif stype == 9:
+        gif87_magic = [47, 49, 46, 38, 37, 61]
+        output = struct.unpack("<" + ("%s" % 'B' * (slen)), data[offset : (offset + slen)])
+
+        gif87 = gif87_magic + list(output)
+
+    	image = open(gif87img.gif, "wb")
+    	image.write(bytearray(gif87))
+    	print ("Image/gif generated %s" % "gif87img.gif")
+ 
+    elif stype == 10:
+        gif89_magic = [47, 49, 46, 38, 39, 61]
+        output = struct.unpack("<" + ("%s" % 'B' * (slen)), data[offset : (offset + slen)])
+
+        gif89 = gif89_magic + list(output)
+
+        image = open(gif89img.gif, "wb")
+        image.write(bytearray(gif89))
+        print ("Image/gif generated %s" % "gif89img.gif")
+
+        
+    offset = offset + slen
+    i = i + 1
+
+
